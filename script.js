b5ed62f7ff816b5c827fde85484b1856765af128
Cecilia Muyide <83959507+ceccy-ope@users.noreply.github.com>
2023-05-08 14:26:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const chess = document.querySelector(".chess");
+const squares = document.querySelectorAll(".square");
+const info =document.getElementById('info')
+
+let chessPiece;
+
+chess.addEventListener("dragstart", (e) => {
+    chessPiece = e.target;
+})
+
+chess.addEventListener("drag", (e) => {
+    info.textContent= 'You are dragging the ' + e.target.id;
+})
+
+squares.forEach(square => {
+    square.addEventListener("dragover", (e)=>{
+        e.preventDefault()
+    })
+    square.addEventListener("drop", (e)=>{
+      e.target.append(chessPiece);
+      info.textContent= ""
+    })
+})
